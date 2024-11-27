5206407619e09115eb5d99adac45fcd00e230672
Antonio Leitao <leitaoto@gmail.com>
2024-11-27 15:33:52 +0100

    added stability

---
+++ examples/utils.py
@@ -0,0 +1,121 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_diagrams(
+    diagrams,
+    colors,
+    plot_only=None,
+    title=None,
+    xy_range=None,
+    labels=None,
+    colormap="default",
+    size=20,
+    ax_color=np.array([0.0, 0.0, 0.0]),
+    diagonal=True,
+    lifetime=False,
+    legend=True,
+    show=False,
+    ax=None
+):
+    ax = ax or plt.gca()
+    plt.style.use(colormap)
+
+    xlabel, ylabel = "Birth", "Death"
+
+    if not isinstance(diagrams, list):
+        # Must have diagrams as a list for processing downstream
+        diagrams = [diagrams]
+
+    if labels is None:
+        # Provide default labels for diagrams if using self.dgm_
+        labels = ["$H_{{{}}}$".format(i) for i , _ in enumerate(diagrams)]
+
+    if plot_only:
+        diagrams = [diagrams[i] for i in plot_only]
+        labels = [labels[i] for i in plot_only]
+
+    if not isinstance(labels, list):
+        labels = [labels] * len(diagrams)
+
+    # Construct copy with proper type of each diagram
+    # so we can freely edit them.
+    diagrams = [dgm.astype(np.float32, copy=True) for dgm in diagrams]
+
+    # find min and max of all visible diagrams
+    concat_dgms = np.concatenate(diagrams).flatten()
+    has_inf = np.any(np.isinf(concat_dgms))
+    finite_dgms = concat_dgms[np.isfinite(concat_dgms)]
+
+    # clever bounding boxes of the diagram
+    if not xy_range:
+        # define bounds of diagram
+        ax_min, ax_max = np.min(finite_dgms), np.max(finite_dgms)
+        x_r = ax_max - ax_min
+
+        # Give plot a nice buffer on all sides.
+        # ax_range=0 when only one point,
+        buffer = 1 if xy_range == 0 else x_r / 5
+
+        x_down = ax_min - buffer / 2
+        x_up = ax_max + buffer
+
+        y_down, y_up = x_down, x_up
+    else:
+        x_down, x_up, y_down, y_up = xy_range
+
+    yr = y_up - y_down
+
+    if lifetime:
+
+        # Don't plot landscape and diagonal at the same time.
+        diagonal = False
+
+        # reset y axis so it doesn't go much below zero
+        y_down = -yr * 0.05
+        y_up = y_down + yr
+
+        # set custom ylabel
+        ylabel = "Lifetime"
+
+        # set diagrams to be (x, y-x)
+        for dgm in diagrams:
+            dgm[:, 1] -= dgm[:, 0]
+
+        # plot horizon line
+        ax.plot([x_down, x_up], [0, 0], c=ax_color)
+
+    # Plot diagonal
+    if diagonal:
+        ax.plot([x_down, x_up], [x_down, x_up], "--", c=ax_color)
+
+    # Plot inf line
+    if has_inf:
+        # put inf line slightly below top
+        b_inf = y_down + yr * 0.95
+        ax.plot([x_down, x_up], [b_inf, b_inf], "--", c="k", label=r"$\infty$")
+
+        # convert each inf in each diagram with b_inf
+        for dgm in diagrams:
+            dgm[np.isinf(dgm)] = b_inf
+
+    # Plot each diagram
+    for dgm, label,color in zip(diagrams, labels, colors):
+
+        # plot persistence pairs
+        ax.scatter(dgm[:, 0], dgm[:, 1], size,color=color, label=label, edgecolor="none")
+
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+
+    ax.set_xlim([x_down, x_up])
+    ax.set_ylim([y_down, y_up])
+    ax.set_aspect('equal', 'box')
+
+    if title is not None:
+        ax.set_title(title)
+
+    if legend is True:
+        ax.legend(loc="lower right",frameon=False,fontsize=7)
+
+    if show is True:
+        plt.show()
